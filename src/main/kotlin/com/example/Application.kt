05d317f3934d051db837b9292282bbbaa70ac88f
betulnecanli <betulnecanli@gmail.com>
2023-02-05 00:34:15 +0300

    StatusPages added.

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -12,4 +12,5 @@ fun Application.module() {
     configureMonitoring()
     configureSerialization()
     configureRouting()
+    configureDefaultHeader()
 }
---
+++ src/main/kotlin/com/example/plugins/StatusPages.kt
@@ -0,0 +1,18 @@
+package com.example.plugins
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+
+
+fun Application.configureStatusPages(){
+    install(StatusPages){
+        status(HttpStatusCode.NotFound){ call, status ->
+            call.respond(
+                status = HttpStatusCode.NotFound,
+                message = "Page not found"
+            )
+        }
+    }
+}
