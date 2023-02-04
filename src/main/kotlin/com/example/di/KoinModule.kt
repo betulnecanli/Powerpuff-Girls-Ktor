a11ec121772fd8b126dbadfe7ff8e28df64e9d8f
betulnecanli <betulnecanli@gmail.com>
2023-02-04 20:53:01 +0300

    koin module done.

--- src/main/kotlin/com/example/di/KoinModule.kt
+++ src/main/kotlin/com/example/di/KoinModule.kt
@@ -1,8 +1,12 @@
 package com.example.di
 
+import com.example.repository.PPGRepository
+import com.example.repository.PPGRepositoryImpl
 import org.koin.dsl.module
 
 
 val koinModule = module {
-
+    single<PPGRepository>{
+        PPGRepositoryImpl()
+    }
 }
