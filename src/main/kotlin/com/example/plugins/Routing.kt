9ad89afb029f7ea4301630fc003dc785d6e0718b
betulnecanli <betulnecanli@gmail.com>
2023-03-28 01:44:33 +0300

    fix images to image

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -4,11 +4,6 @@ import com.example.routes.characters
 import com.example.routes.root
 import com.example.routes.searchCharacters
 import io.ktor.server.routing.*
-import io.ktor.server.response.*
-import io.ktor.server.resources.*
-import io.ktor.resources.*
-import io.ktor.server.resources.Resources
-import kotlinx.serialization.Serializable
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 
@@ -17,8 +12,8 @@ fun Application.configureRouting() {
         root()
         characters()
         searchCharacters()
-        static("/images") {
-            resources("/images")
+        static("/image") {
+            resources("/image")
         }
     }
 }
